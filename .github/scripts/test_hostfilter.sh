a5ca2bf94d7f6ec66d77e5fa21b460d8947178bd
Dan Fornika <dfornika@gmail.com>
2020-12-16 17:31:08 -0800

    Start to customize test_hostfilter script

--- .github/scripts/test_hostfilter.sh
+++ .github/scripts/test_hostfilter.sh
@@ -4,27 +4,19 @@ export PATH=/opt/conda/bin:$PATH
 
 git clone https://github.com/BCCDC-PHL/artic-ncov2019.git
 
-export $REF_FILE=artic-ncov2019/primer_schemes/nCoV-2019/V1200/nCoV-2019.reference.fasta
-export $bed_FILE=artic-ncov2019/primer_schemes/nCoV-2019/V1200/nCoV-2019.bed
+export $REF_FILE=$GITHUB_WORKSPACE/.github/data/fastas/composite_nCoV-2019_chr1_regions.fa
 echo ref file: $REF_FILE
-echo bed file: $BED_FILE
 
-# run current pull request code
-# --sanger profile: there are only 2 available cpus in the github runner execution
-sed -i s'/cpus = 4/cpus = 2/'g conf/coguk/sanger.config
-singularity --version
-echo Nextflow run --illumina mode with --ref, --bed --cram and outCram... >> artifacts/test_artifact.log
-NXF_VER=20.03.0-edge nextflow run ./main.nf \
+echo Nextflow run tests... >> artifacts/test_artifact.log
+NXF_VER=20.10.0 nextflow run ./test.nf \
        -profile conda \
-       --cache 
-       --ref $REF_FILE \
-       --bed $BED_FILE \
-       --cram \
-       --outCram \
-       --directory $PWD/.github/data/crams/ \
+       --cache $REPO/conda_cache_dir \
+       --composite_ref $REF_FILE \
+       --fastqSearchPath $GITHUB_WORKSPACE/.github/data/fastqs
        --illumina \
-       --prefix test
-cp .nextflow.log artifacts/Outcram_ref_bed.nextflow.log
+       --viral_contig_name 'MN908947.3:1-500' \
+       --outdir test_hostfilter_out
+cp .nextflow.log artifacts/nextflow_tests.nextflow.log
 
 # get first cram file that macthes in given folder 
 cram_exists=($(find results/*_bamToCram -name '*.cram'))
